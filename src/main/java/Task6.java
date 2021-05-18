b0234e7053475b89a89f76dc78a8aaf44d0be805
Alekzanderor <alekzanderor@gmail.com>
2021-05-19 01:05:16 +0300

    Задание 6 v1

---
+++ src/main/java/Task6.java
@@ -0,0 +1,34 @@
+import GiftTreats.Treat;
+
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+import java.util.stream.Stream;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
+
+
+public class Task6 {
+    public static void main(String[] args) throws IOException {
+
+
+        List <String> listtry = Files.readAllLines(Paths.get("abc.txt"))
+                .stream()
+                .map(l -> l.split(" "))
+                .flatMap(Arrays::stream)
+                .sorted()
+                .collect(Collectors.toList());
+
+        System.out.println(listtry);
+
+        Map<String,Integer> counter = new HashMap<>();
+
+        for (String x : listtry) {
+            int newValue = counter.getOrDefault(x, 0) + 1;
+            counter.put(x, newValue);
+        }
+
+        System.out.println(counter);
+
+    }}
